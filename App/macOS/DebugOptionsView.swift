3af59adbb50d5a11cd1c0cd139b61d7fcf2ecb5a
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-05-28 16:09:14 +0200

    feature(ARView): improve debug help

--- App/macOS/DebugOptionsView.swift
+++ App/macOS/DebugOptionsView.swift
@@ -23,17 +23,25 @@ public struct DebugOptionsView: View {
         Section(
           content: {
             Toggle("Anchor Geometry", isOn: viewStore.binding(\.$showAnchorGeometry))
+              .help("Display anchor geometry.")
             Toggle("Anchor Origins", isOn: viewStore.binding(\.$showAnchorOrigins))
+              .help("Display anchor origins.")
             Toggle("Feature Points", isOn: viewStore.binding(\.$showFeaturePoints))
+              .help(
+                "Display a point cloud showing intermediate results of the scene analysis used to track device position."
+              )
             Toggle("Physics", isOn: viewStore.binding(\.$showPhysics))
+              .help("Draw visualizations for collision objects and rigid bodies.")
             //TODO: find a way to verify this value
-            // Toggle(
-            //   "Scene Reconstruction",
-            //   isOn: viewStore.binding(\.$showSceneUnderstanding)
-            // )
-            //.disabled(!ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh))
+            // Toggle("Scene Reconstruction", isOn: viewStore.binding(\.$showSceneUnderstanding))
+            //   .help("Display the depth-colored wireframe for scene understanding meshes.")
+            //   .disabled(!ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh))
             Toggle("Statistics", isOn: viewStore.binding(\.$showStatistics))
+              .help("Collect performance statistics and display them in the view.")
             Toggle("World Origin", isOn: viewStore.binding(\.$showWorldOrigin))
+              .help(
+                "Display a coordinate axis indicating the position and orientation of the AR world coordinate system."
+              )
           },
           header: {
             Text("ARView Debug Options")
