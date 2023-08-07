8f6ade0d52b9499b4b355ec99f64fee07160c45d
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-08-07 11:02:08 +0200

    feature: described more properties on raw dump

--- Sources/RealityDumpClient/Live.swift
+++ Sources/RealityDumpClient/Live.swift
@@ -84,12 +84,12 @@ extension RealityKit.Entity: CustomDumpReflectable {
         ///Components
         "components": self.components,
         /// Synchronization
-        //TODO: "synchronization": self.synchronization,
+        "synchronization": self.synchronization,
         "isOwner": self.isOwner,
         ///Nearest Anchor
         //TODO: "anchor": self.anchor,
         ///Animations
-        //TODO: "availableAnimations": self.availableAnimations,
+        "availableAnimations": self.availableAnimations,
         ///Animating an Entity
         //TODO: "defaultAnimationClock": self.defaultAnimationClock,
         //TODO: "bindableValues": self.bindableValues,
@@ -106,3 +106,43 @@ extension RealityKit.Entity: CustomDumpReflectable {
     )
   }
 }
+
+extension RealityKit.AnimationResource: CustomDumpReflectable {
+  public var customDumpMirror: Mirror {
+    .init(
+      self,
+      children: [
+        "name": self.name,
+        "definition": self.definition,
+      ]
+    )
+  }
+}
+
+//TODO: describe the conforming types of AnimationDefinition
+//https://developer.apple.com/documentation/realitykit/animationdefinition
+//AnimationGroup
+//AnimationView
+//BlendTreeAnimation
+//FromToByAnimation
+//OrbitAnimation
+//SampledAnimation
+//extension RealityKit.AnimationDefinition: CustomDumpReflectable {
+//  public var customDumpMirror: Mirror {
+//    .init(
+//      self,
+//      children: [
+//        "name": self.name,
+//        "bindTarget": self.bindTarget,
+//        "blendLayer": self.blendLayer,
+//        "speed": self.speed,
+//        "delay": self.delay,
+//        "duration": self.duration,
+//        "offset": self.offset,
+//        "trimDuration": self.trimDuration,
+//        "trimStart": self.trimStart,
+//        "trimEnd": self.trimEnd,
+//      ]
+//    )
+//  }
+//}
