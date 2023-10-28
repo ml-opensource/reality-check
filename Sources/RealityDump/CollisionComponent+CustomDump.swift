ccd7ac987d80d2081ec8f8a3443385c79d5f1faf
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-10-28 11:50:37 +0200

    wip

---
+++ Sources/RealityDump/CollisionComponent+CustomDump.swift
@@ -0,0 +1,9 @@
+import RealityKit
+import CustomDump
+
+//extension CollisionComponent: CustomDumpStringConvertible {
+//  public var customDumpDescription: String {
+//    // TODO: find if is possible to access
+//    // interactionType: CollisionComponent.InteractionType(rawValue: 0)
+//  }
+//}
