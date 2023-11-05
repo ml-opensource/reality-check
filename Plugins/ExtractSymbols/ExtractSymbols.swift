143dbe90ba9c3eb7b01dd90e6e1e076697220df0
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-11-05 19:08:06 +0100

    cleanup

--- Plugins/ExtractSymbols/ExtractSymbols.swift
+++ Plugins/ExtractSymbols/ExtractSymbols.swift
@@ -4,10 +4,10 @@ import PackagePlugin
 @main
 struct ExtractSymbols: CommandPlugin {
   func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
-    // try extractSymbolGraph(from: xcodePath(), context: context)
-    // try processSymbols(context: context)
-    // try generateModels(context: context)
-    // try generateMirrors(context: context)
+    try extractSymbolGraph(from: xcodePath(), context: context)
+    try processSymbols(context: context)
+    try generateModels(context: context)
+    try generateMirrors(context: context)
     try generateCodable(context: context)
   }
 }
@@ -108,7 +108,7 @@ extension ExtractSymbols {
   }
 }
 
-//MARK: - Generate CustomDump
+//MARK: - Generate Mirrors
 
 extension ExtractSymbols {
   func generateMirrors(context: PackagePlugin.PluginContext) throws {
