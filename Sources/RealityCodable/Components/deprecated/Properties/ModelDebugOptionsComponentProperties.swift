dc6cc74b1a608ea6bd32190dad711726453c2e64
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-11-08 21:01:40 +0100

    cleanup: remove some deprecated code

--- Sources/RealityCodable/Components/deprecated/Properties/ModelDebugOptionsComponentProperties.swift
+++
@@ -1,9 +0,0 @@
-import RealityKit
-
-public struct ModelDebugOptionsComponentProperties: ComponentPropertiesRepresentable {
-	/// The part of the rendering process to display as the entity’s surface
-	/// texture.
-	public let visualizationMode: ModelDebugOptionsComponent.VisualizationMode
-}
-
-extension ModelDebugOptionsComponent.VisualizationMode: Codable {}
