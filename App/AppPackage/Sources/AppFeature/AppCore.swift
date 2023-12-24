c0370c812d4cc315fdceefa23cc0d18589d04ac7
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-12-24 19:14:11 +0100

    feature: improve hot cross-platform reconnection

--- App/AppPackage/Sources/AppFeature/AppCore.swift
+++ App/AppPackage/Sources/AppFeature/AppCore.swift
@@ -14,7 +14,7 @@ public enum Layout {
 @Reducer
 public struct AppCore {
   public init() {}
-
+  
   @ObservableState
   public struct State: Equatable {
     public var entitiesNavigator: EntitiesNavigator.State?
@@ -27,7 +27,7 @@ public struct AppCore {
     public var multipeerConnection: MultipeerConnection.State
     public var selectedSection: Section
     public var viewPortSize: CGSize
-
+    
     public init(
       entitiesNavigator: EntitiesNavigator.State? = nil,
       isConnectionSetupPresented: Bool = true,
@@ -52,7 +52,7 @@ public struct AppCore {
       self.viewPortSize = viewPortSize
     }
   }
-
+  
   @CasePathable
   public enum Action: BindableAction {
     case binding(BindingAction<State>)
@@ -60,92 +60,98 @@ public struct AppCore {
     case multipeerConnection(MultipeerConnection.Action)
     case updateViewportSize(CGSize)
   }
-
+  
   public enum Section {
     case arView
     case entities
   }
-
+  
   @Dependency(\.streamingClient) var streamingClient
-
+  
   public var body: some ReducerOf<Self> {
     BindingReducer()
-
+    
     Scope(state: \.multipeerConnection, action: \.multipeerConnection) {
       MultipeerConnection()
     }
-
+    
     Reduce<State, Action> { state, action in
       switch action {
-        case .binding(_):
-          return .none
-
-        case .entitiesNavigator(.iOS(.delegate(.didSelectEntity(let entityID)))),
+      case .binding(_):
+        return .none
+        
+      case .entitiesNavigator(.iOS(.delegate(.didSelectEntity(let entityID)))),
           .entitiesNavigator(.visionOS(.delegate(.didSelectEntity(let entityID)))):
-          return .send(.multipeerConnection(.sendSelection(entityID)))
-
-        case .entitiesNavigator(
-          .iOS(.arViewSection(.delegate(.didUpdateDebugOptions(let options))))
-        ):
-          return .send(.multipeerConnection(.sendDebugOptions(options)))
-
-        case .entitiesNavigator:
+        return .send(.multipeerConnection(.sendSelection(entityID)))
+        
+      case .entitiesNavigator(
+        .iOS(.arViewSection(.delegate(.didUpdateDebugOptions(let options))))
+      ):
+        return .send(.multipeerConnection(.sendDebugOptions(options)))
+        
+      case .entitiesNavigator:
+        return .none
+        
+      case .multipeerConnection(.delegate(.didUpdateSessionState(.notConnected))):
+        if state.multipeerConnection.isManuallyDisconnected {
+          state.entitiesNavigator = nil
+          state.isInspectorDisplayed = false
+        }
+        return .none
+        
+      case .multipeerConnection(.delegate(.didUpdateSessionState(.connected))):
+        state.entitiesNavigator = nil
+        state.isInspectorDisplayed = false
+        return .none
+        
+        
+      case .multipeerConnection(.delegate(.peersUpdated)):
+        /// Display "Connection Setup" dialog when not connected to any peer but theres at least one available
+        if !state.multipeerConnection.peers.isEmpty,
+           state.multipeerConnection.sessionState == .notConnected
+        {
+          state.isConnectionSetupPresented = true
+        }
+        return .none
+        
+      case .multipeerConnection(.delegate(.receivedDump(let dump))):
+        switch state.entitiesNavigator {
+        case .some(.iOS):
+          return .send(.entitiesNavigator(.iOS(.dumpOutput(dump))))
+        case .some(.visionOS):
+          return .send(.entitiesNavigator(.visionOS(.dumpOutput(dump))))
+        case .none:
           return .none
-
-        case .multipeerConnection(.delegate(.didUpdateSessionState(.notConnected))):
-          if state.multipeerConnection.isManuallyDisconnected {
-            state.entitiesNavigator = nil
-            state.isInspectorDisplayed = false
-          }
-          return .none
-
-        case .multipeerConnection(.delegate(.peersUpdated)):
-          /// Display "Connection Setup" dialog when not connected to any peer but theres at least one available
-          if !state.multipeerConnection.peers.isEmpty,
-            state.multipeerConnection.sessionState == .notConnected
-          {
-            state.isConnectionSetupPresented = true
-          }
-          return .none
-
-        case .multipeerConnection(.delegate(.receivedDump(let dump))):
-          switch state.entitiesNavigator {
-            case .some(.iOS):
-              return .send(.entitiesNavigator(.iOS(.dumpOutput(dump))))
-            case .some(.visionOS):
-              return .send(.entitiesNavigator(.visionOS(.dumpOutput(dump))))
-            case .none:
-              return .none
-          }
-
-        case .multipeerConnection(.delegate(.receivedDecodedARView(let decodedARView))):
-          if state.entitiesNavigator == nil {
-            state.entitiesNavigator = .iOS(
-              .init(arViewSection: .init(arView: decodedARView))
-            )
-          }
-          state.isInspectorDisplayed = true
-          return .send(.entitiesNavigator(.iOS(.refreshEntities)))
-
-        case .multipeerConnection(.delegate(.receivedDecodedScene(let decodedScene))):
-          let entities = decodedScene.children.map(\.value)
-          if state.entitiesNavigator == nil {
-            state.entitiesNavigator = .visionOS(.init(entities))
-          }
-          state.isInspectorDisplayed = true
-          return .send(.entitiesNavigator(.visionOS(.refreshEntities(entities))))
-
-        case .multipeerConnection(.delegate(.receivedVideoFrameData(let videoFrameData))):
-          state.isStreaming = true
-          streamingClient.prepareForRender(videoFrameData)
-          return .none
-
-        case .multipeerConnection(_):
-          return .none
-
-        case .updateViewportSize(let size):
-          state.viewPortSize = size
-          return .none
+        }
+        
+      case .multipeerConnection(.delegate(.receivedDecodedARView(let decodedARView))):
+        if state.entitiesNavigator == nil {
+          state.entitiesNavigator = .iOS(
+            .init(arViewSection: .init(arView: decodedARView))
+          )
+        }
+        state.isInspectorDisplayed = true
+        return .send(.entitiesNavigator(.iOS(.refreshEntities)))
+        
+      case .multipeerConnection(.delegate(.receivedDecodedScene(let decodedScene))):
+        let entities = decodedScene.children.map(\.value)
+        if state.entitiesNavigator == nil {
+          state.entitiesNavigator = .visionOS(.init(entities))
+        }
+        state.isInspectorDisplayed = true
+        return .send(.entitiesNavigator(.visionOS(.refreshEntities(entities))))
+        
+      case .multipeerConnection(.delegate(.receivedVideoFrameData(let videoFrameData))):
+        state.isStreaming = true
+        streamingClient.prepareForRender(videoFrameData)
+        return .none
+        
+      case .multipeerConnection(_):
+        return .none
+        
+      case .updateViewportSize(let size):
+        state.viewPortSize = size
+        return .none
       }
     }
     .ifLet(\.entitiesNavigator, action: \.entitiesNavigator) {
